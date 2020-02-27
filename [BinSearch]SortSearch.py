28b6be35aaf2aa8a3ce96d50884c046c4b18e3b5
Nhatcao99 <caominhnhat99@gmail.com>
2020-02-27 09:56:32 +0700

    Speed 94 Space 100

---
+++ [BinSearch]SortSearch.py
@@ -0,0 +1,18 @@
+def search(self, nums: List[int], target: int) -> int:
+    def BinSearch(nums: List[int], start: int, end: int, target: int):
+        result = -1
+        if start == end:
+            if nums[start] == target:
+                return start
+            else:
+                return -1
+        mid = (start + end) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] > target:
+            return BinSearch(nums, start, mid, target)
+        else:
+            return BinSearch(nums, mid + 1, end, target)
+
+    n = len(nums)
+    return BinSearch(nums, 0, n - 1, target)
